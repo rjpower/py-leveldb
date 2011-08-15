2bb76bcbdbc1f137873f445ab3a8cb51b7d06869
arnimarj@gmail.com <arnimarj@gmail.com@72a8a67b-58ad-ce6a-cf8f-6a8106eb947b>
2011-08-15 17:54:45 +0000

    A script to download and build different Python versions.
    git-svn-id: http://py-leveldb.googlecode.com/svn/trunk@19 72a8a67b-58ad-ce6a-cf8f-6a8106eb947b

---
+++ versions/download.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# see http://python.org/download/releases/
+
+wget -c http://python.org/ftp/python/3.2.1/Python-3.2.1.tar.bz2
+wget -c http://python.org/ftp/python/3.1.4/Python-3.1.4.tar.bz2
+wget -c http://python.org/ftp/python/3.0.1/Python-3.0.1.tar.bz2
+wget -c http://python.org/ftp/python/2.7.2/Python-2.7.2.tar.bz2
+wget -c http://python.org/ftp/python/2.6.7/Python-2.6.7.tar.bz2
+wget -c http://python.org/ftp/python/2.5.6/Python-2.5.6.tar.bz2
+wget -c http://python.org/ftp/python/2.4.6/Python-2.4.6.tar.bz2
+
+for i in `ls | grep bz2$`; do bzip2 -dc $i | tar -x; done
+
+for i in `find -maxdepth 1 -type d | grep ^./P | sort`; do
+(
+	cd $i;
+	./configure --prefix=`pwd`-build;
+	make && make install;
+)
+done
