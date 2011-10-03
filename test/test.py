bde930d2caaad65163262ebb8b480d5a5ac6022d
arnimarj@gmail.com <arnimarj@gmail.com@72a8a67b-58ad-ce6a-cf8f-6a8106eb947b>
2011-10-03 19:00:57 +0000

    Tests.
    git-svn-id: http://py-leveldb.googlecode.com/svn/trunk@32 72a8a67b-58ad-ce6a-cf8f-6a8106eb947b

--- test/test.py
+++ test/test.py
@@ -5,94 +5,179 @@
 
 import sys, string
 
-def print_usage_exit():
-	sys.exit('usage: ./test.py db_dir import_dir')
+from twisted.internet import defer, threads
+from twisted.trial import unittest
 
-def main():
-	if len(sys.argv) != 3:
-		print_usage_exit()
+class TestLevelDB(unittest.TestCase):
+	def setUp(self):
+		# import local leveldb
+		p = sys.path
 
-	db_dir = sys.argv[1]
-	import_dir = sys.argv[2]
+		try:
+			sys.path = ['/home/arni/py-leveldb/build/lib.linux-x86_64-2.7']
+			import leveldb as _leveldb
+			self.leveldb = _leveldb
+		finally:
+			sys.path = p
 
-	p = sys.path
-	sys.path = [import_dir]
-	import leveldb
-	sys.path = p
+		# destroy previous database, if any
+		self.name = 'db_a'
+		self.leveldb.DestroyDB(self.name)
 
-	db = leveldb.LevelDB(db_dir)
+	def _open(self, create_if_missing = True, error_if_exists = False):
+		options = {
+			'create_if_missing': True,
+			'error_if_exists': error_if_exists,
+			'paranoid_checks': False,
+			'block_cache_size': 8 * (2 << 20),
+			'write_buffer_size': 2 * (2 << 20),
+			'block_size': 4096,
+			'max_open_files': 1000,
+			'block_restart_interval': 16
+		}
 
-	# first test
-	clear(db)
-	insert_lowercase(db)
-	assert(len(list(db.RangeIter())) == 26)
-	test_lowercase_iter(db)
-	test_lowercase_get(db)
+		return self.leveldb.LevelDB(self.name, **options)
 
-	# clear databse
-	clear_batch(leveldb.WriteBatch(), db)
-	insert_uppercase_batch(leveldb.WriteBatch(), db)
-	test_uppercase_iter(db)
-	test_uppercase_get(db)
-	assert(len(list(db.RangeIter())) == 26)
+	# NOTE: modeled after test 'Snapshot'
+	def testSnapshotBasic(self):
+		db = self._open()
 
-def insert_lowercase(db):
-	for c in string.lowercase:
-		db.Put(c, 'hello')
+		# destroy database, if any
+		db.Put('foo', 'v1')
+		s1 = db.CreateSnapshot()
 
-def test_lowercase_get(db):
-	for k in string.lowercase:
-		v = db.Get(k)
-		assert(v == 'hello')
-		assert(k in string.lowercase)
+		db.Put('foo', 'v2')
+		s2 = db.CreateSnapshot()
 
-def test_lowercase_iter(db):
-	s = ''.join(k for k, v in db.RangeIter('j', 'm'))
-	assert(s == 'jklm')
+		db.Put('foo', 'v3')
+		s3 = db.CreateSnapshot()
 
-	s = ''.join(k for k, v in db.RangeIter('s'))
-	assert(s == 'stuvwxyz')
+		db.Put('foo', 'v4')
 
-	s = ''.join(k for k, v in db.RangeIter(key_to = 'e'))
-	assert(s == 'abcde')
+		self.assertEquals(s1.Get('foo'), 'v1')
+		self.assertEquals(s2.Get('foo'), 'v2')
+		self.assertEquals(s3.Get('foo'), 'v3')
+		self.assertEquals(db.Get('foo'), 'v4')
 
-def clear(db):
-	for k in db.RangeIter(include_value = False):
-		db.Delete(k)
+		# TBD: close properly
+		del s3
+		self.assertEquals(s1.Get('foo'), 'v1')
+		self.assertEquals(s2.Get('foo'), 'v2')
+		self.assertEquals(db.Get('foo'), 'v4')
 
-def clear_batch(batch, db):
-	for k in db.RangeIter(include_value = False):
-		batch.Delete(k)
+		# TBD: close properly
+		del s1
+		self.assertEquals(s2.Get('foo'), 'v2')
+		self.assertEquals(db.Get('foo'), 'v4')
 
-	db.Write(batch)
+		# TBD: close properly
+		del s2
+		self.assertEquals(db.Get('foo'), 'v4')
 
-def insert_uppercase_batch(batch, db):
-	for c in string.uppercase:
-		batch.Put(c, 'hello')
+		# re-open
+		del db
+		db = self._open()
+		self.assertEquals(db.Get('foo'), 'v4')
 
-	db.Write(batch)
+	def ClearDB(self, db):
+		for k in list(db.RangeIter(include_value = False)):
+			db.Delete(k)
 
-def test_uppercase_iter(db):
-	s = ''.join(k for k, v in db.RangeIter('J', 'M'))
-	assert(s == 'JKLM')
+	def ClearDB_batch(self, db):
+		b = self.leveldb.WriteBatch()
 
-	s = ''.join(k for k, v in db.RangeIter('S'))
-	assert(s == 'STUVWXYZ')
+		for k in db.RangeIter(include_value = False):
+			b.Delete(k)
 
-	s = ''.join(k for k, v in db.RangeIter(key_to = 'E'))
-	assert(s == 'ABCDE')
+		db.Write(b)
 
-def test_uppercase_get(db):
-	for k in string.uppercase:
-		v = db.Get(k)
-		assert(v == 'hello')
-		assert(k in string.uppercase)
+	def CountDB(self, db):
+		return sum(1 for i in db.RangeIter())
 
-if __name__ == '__main__':
-	main()
+	def _insert_lowercase(self, db):
+		b = self.leveldb.WriteBatch()
 
-#db = leveldb.LevelDB('./db')
-#db.Put('a', 'b' * 100, sync = False)
-#print db.Get('a')
-#del db
-#leveldb.RepairDB('./db')
+		for c in string.lowercase:
+			b.Put(c, 'hello')
+
+		db.Write(b)
+
+	def _insert_uppercase_batch(self, db):
+		b = self.leveldb.WriteBatch()
+
+		for c in string.uppercase:
+			b.Put(c, 'hello')
+
+		db.Write(b)
+
+	def _test_uppercase_get(self, db):
+		for k in string.uppercase:
+			v = db.Get(k)
+			self.assertEquals(v, 'hello')
+			self.assert_(k in string.uppercase)
+
+	def _test_uppercase_iter(self, db):
+		s = ''.join(k for k, v in db.RangeIter('J', 'M'))
+		self.assertEquals(s, 'JKLM')
+
+		s = ''.join(k for k, v in db.RangeIter('S'))
+		self.assertEquals(s, 'STUVWXYZ')
+
+		s = ''.join(k for k, v in db.RangeIter(key_to = 'E'))
+		self.assertEquals(s, 'ABCDE')
+
+	def _test_lowercase_iter(self, db):
+		s = ''.join(k for k, v in db.RangeIter('j', 'm'))
+		self.assertEquals(s, 'jklm')
+
+		s = ''.join(k for k, v in db.RangeIter('s'))
+		self.assertEquals(s, 'stuvwxyz')
+
+		s = ''.join(k for k, v in db.RangeIter(key_to = 'e'))
+		self.assertEquals(s, 'abcde')
+
+	def _test_lowercase_get(self, db):
+		for k in string.lowercase:
+			v = db.Get(k)
+			self.assertEquals(v, 'hello')
+			self.assert_(k in string.lowercase)
+
+	def testIterationBasic(self):
+		db = self._open()
+		self._insert_lowercase(db)
+		self.assertEquals(self.CountDB(db), 26)
+		self._test_lowercase_iter(db)
+		self._test_lowercase_get(db)
+		self.ClearDB_batch(db)
+		self._insert_uppercase_batch(db)
+		self._test_uppercase_iter(db)
+		self._test_uppercase_get(db)
+		self.assertEquals(self.CountDB(db), 26)
+
+	# tried to re-produce http://code.google.com/p/leveldb/issues/detail?id=44
+	def testMe(self):
+		db = self._open()
+		db.Put('key1', 'val1')
+		del db
+		db = self._open()
+		db.Delete('key2')
+		db.Delete('key1')
+		del db
+		db = self._open()
+		db.Delete('key2')
+		del db
+		db = self._open()
+		db.Put('key3', 'val1')
+		del db
+		db = self._open()
+		del db
+		db = self._open()
+		v = list(db.RangeIter())
+		self.assertEquals(v, [('key3', 'val1')])
+
+	def testOpenSame(self):
+		a = self._open()
+		a.Put('foo', 'bar')
+
+		# this one should fail, we need to fix this in the bindings
+		b = self._open()
