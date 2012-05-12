991ba304a3a1333f5447aa1116b274a15e2f8b89
arnimarj@gmail.com <arnimarj@gmail.com@72a8a67b-58ad-ce6a-cf8f-6a8106eb947b>
2012-05-12 22:44:59 +0000

    more test setup
    git-svn-id: http://py-leveldb.googlecode.com/svn/trunk@42 72a8a67b-58ad-ce6a-cf8f-6a8106eb947b

--- versions/download.sh
+++ versions/download.sh
@@ -10,23 +10,23 @@ set -e
 
 	# see http://python.org/download/releases/
 
-#	wget -qc http://python.org/ftp/python/3.2.3/Python-3.2.3.tar.bz2;
-#	wget -qc http://python.org/ftp/python/3.1.5/Python-3.1.5.tar.bz2;
-#	wget -qc http://python.org/ftp/python/3.0.1/Python-3.0.1.tar.bz2;
-#	wget -qc http://python.org/ftp/python/2.7.3/Python-2.7.3.tar.bz2;
-#	wget -qc http://python.org/ftp/python/2.6.8/Python-2.6.8.tar.bz2;
-#	wget -qc http://python.org/ftp/python/2.5.6/Python-2.5.6.tar.bz2;
-#	wget -qc http://python.org/ftp/python/2.4.6/Python-2.4.6.tar.bz2;
+	wget -qc http://python.org/ftp/python/3.2.3/Python-3.2.3.tar.bz2;
+	wget -qc http://python.org/ftp/python/3.1.5/Python-3.1.5.tar.bz2;
+	wget -qc http://python.org/ftp/python/3.0.1/Python-3.0.1.tar.bz2;
+	wget -qc http://python.org/ftp/python/2.7.3/Python-2.7.3.tar.bz2;
+	wget -qc http://python.org/ftp/python/2.6.8/Python-2.6.8.tar.bz2;
+	wget -qc http://python.org/ftp/python/2.5.6/Python-2.5.6.tar.bz2;
+	wget -qc http://python.org/ftp/python/2.4.6/Python-2.4.6.tar.bz2;
 
 	# decompress
-#	for i in `ls | grep bz2$`; do bzip2 -dc $i | tar -x; done
+	for i in `ls | grep bz2$`; do bzip2 -dc $i | tar -x; done
 
-#	# apply patches
-#	for i in `find -maxdepth 1 -type d | grep ^./P | grep -v build | grep -v env | sort`; do
-#	(
-#		cd $i;
-#		patch -s < ../../setup.py.patch;
-#	) done
+	# apply patches
+	for i in `find -maxdepth 1 -type d | grep ^./P | grep -v build | grep -v env | sort`; do
+	(
+		cd $i;
+		patch -s < ../../setup.py.patch;
+	) done
 
 	# build
 	for i in `find -maxdepth 1 -type d | grep ^./P | grep -v build | grep -v env | sort`; do
@@ -38,11 +38,11 @@ set -e
 		make install > /dev/null;
 	) done
 
-#	# virtualenv
-#	for i in `find -maxdepth 1 -type d | grep ^./P | grep -v build | grep -v env | sort`; do
-#	(
-#		echo $i;
-#		virtualenv -p $i-build/bin/python $i-env;
-#	) done
+	# virtualenv
+	for i in `find -maxdepth 1 -type d | grep ^./P | grep -v build | grep -v env | sort`; do
+	(
+		echo $i;
+		virtualenv -p $i-build/bin/python $i-env || virtualenv -p $i-build/bin/python3.0 $i-env || virtualenv -p $i-build/bin/python3 $i-env;
+	) done
 
 )
