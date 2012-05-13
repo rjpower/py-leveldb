6cf702d7b5c95cede5f3a36ea479ded0289cf2cd
arnimarj@gmail.com <arnimarj@gmail.com@72a8a67b-58ad-ce6a-cf8f-6a8106eb947b>
2012-05-13 12:06:29 +0000

    test script fix
    git-svn-id: http://py-leveldb.googlecode.com/svn/trunk@44 72a8a67b-58ad-ce6a-cf8f-6a8106eb947b

--- versions/download.sh
+++ versions/download.sh
@@ -8,42 +8,43 @@ set -e
 	mkdir -p download;
 	cd download;
 
-#	# see http://python.org/download/releases/
-#
-#	wget -qc http://python.org/ftp/python/3.2.3/Python-3.2.3.tar.bz2;
-#	wget -qc http://python.org/ftp/python/3.1.5/Python-3.1.5.tar.bz2;
-#	wget -qc http://python.org/ftp/python/3.0.1/Python-3.0.1.tar.bz2;
-#	wget -qc http://python.org/ftp/python/2.7.3/Python-2.7.3.tar.bz2;
-#	wget -qc http://python.org/ftp/python/2.6.8/Python-2.6.8.tar.bz2;
-#	wget -qc http://python.org/ftp/python/2.5.6/Python-2.5.6.tar.bz2;
-#	wget -qc http://python.org/ftp/python/2.4.6/Python-2.4.6.tar.bz2;
-#
-#	# decompress
-#	for i in `ls | grep bz2$`; do bzip2 -dc $i | tar -x; done
-#
-#	# apply patches
-#	for i in `find -maxdepth 1 -type d | grep ^./P | grep -v build | grep -v env | sort`; do
-#	(
-#		cd $i;
-#		patch -s < ../../setup.py.patch;
-#	) done
-#
-#	# build
-#	for i in `find -maxdepth 1 -type d | grep ^./P | grep -v build | grep -v env | sort`; do
-#	(
-#		cd $i;
-#		pwd;
-#		CXX=g++ ./configure --disable-option-checking --enable-unicode=ucs4 --with-wide-unicode --prefix=`pwd`-build > /dev/null;
-#		make         > /dev/null;
-#		make install > /dev/null;
-#	) done
+	# see http://python.org/download/releases/
+
+	# 2.4 and 3.* not supported yet
+	#wget -qc http://python.org/ftp/python/3.2.3/Python-3.2.3.tar.bz2;
+	#wget -qc http://python.org/ftp/python/3.1.5/Python-3.1.5.tar.bz2;
+	#wget -qc http://python.org/ftp/python/3.0.1/Python-3.0.1.tar.bz2;
+	wget -qc http://python.org/ftp/python/2.7.3/Python-2.7.3.tar.bz2;
+	wget -qc http://python.org/ftp/python/2.6.8/Python-2.6.8.tar.bz2;
+	wget -qc http://python.org/ftp/python/2.5.6/Python-2.5.6.tar.bz2;
+	#wget -qc http://python.org/ftp/python/2.4.6/Python-2.4.6.tar.bz2;
+
+	# decompress
+	for i in `ls | grep bz2$`; do bzip2 -dc $i | tar -x; done
+
+	# apply patches
+	for i in `find -maxdepth 1 -type d | grep ^./P | grep -v build | grep -v env | sort`; do
+	(
+		cd $i;
+		patch -s < ../../setup.py.patch;
+	) done
+
+	# build
+	for i in `find -maxdepth 1 -type d | grep ^./P | grep -v build | grep -v env | sort`; do
+	(
+		cd $i;
+		pwd;
+		CXX=g++ ./configure --disable-option-checking --enable-unicode=ucs4 --with-wide-unicode --prefix=`pwd`-build > /dev/null;
+		make         > /dev/null;
+		make install > /dev/null;
+	) done
 
 	# virtualenv
-	for i in `find -maxdepth 1 -type d | grep ^./P | grep -v build | grep -v env | grep -v Python-2.4 | grep -v Python-3 | sort`; do
+	for i in `find -maxdepth 1 -type d | grep ^./P | grep -v build | grep -v env | sort`; do
 	(
 		echo $i;
 		unset PYTHONDONTWRITEBYTECODE;
-		#(virtualenv -p $i-build/bin/python $i-env || virtualenv -p $i-build/bin/python3.0 $i-env || virtualenv -p $i-build/bin/python3 $i-env) > /dev/null;
+		(virtualenv -p $i-build/bin/python $i-env || virtualenv -p $i-build/bin/python3.0 $i-env || virtualenv -p $i-build/bin/python3 $i-env) > /dev/null;
 		source $i-env/bin/activate;
 		pip -q install gevent twisted;
 	) done
