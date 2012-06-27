ca509f5b492562c2206ae404c6c18e992feb677c
arnimarj@gmail.com <arnimarj@gmail.com@72a8a67b-58ad-ce6a-cf8f-6a8106eb947b>
2012-06-27 19:11:55 +0000

    forgot the commit my simple test script
    git-svn-id: http://py-leveldb.googlecode.com/svn/trunk@54 72a8a67b-58ad-ce6a-cf8f-6a8106eb947b

---
+++ versions/test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+(
+	cd download;
+
+	# virtualenv
+	for i in `find -maxdepth 1 -type d | grep ^./P | grep -v build | grep -v env | sort`; do
+	(
+		echo $i;
+		unset PYTHONDONTWRITEBYTECODE;
+		source $i-env/bin/activate;
+		(
+			cd ../..;
+			python setup.py build --force > /dev/null;
+			python setup.py install > /dev/null;
+			(
+				cd test;
+				pwd;
+				python test.py;
+			)
+		)
+	) done
+)
