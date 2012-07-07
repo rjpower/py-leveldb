d6390e2d3f7e7f7dfcd39374c8728a53b67f5497
arnimarj@gmail.com <arnimarj@gmail.com@72a8a67b-58ad-ce6a-cf8f-6a8106eb947b>
2012-07-07 21:50:20 +0000

    Fixed a compilation bug. lib-leveldb was sometimes not statically linked. Thanks to Jay Ridge.
    git-svn-id: http://py-leveldb.googlecode.com/svn/trunk@55 72a8a67b-58ad-ce6a-cf8f-6a8106eb947b

--- compile_leveldb.sh
+++ compile_leveldb.sh
@@ -15,5 +15,5 @@ set -e;
 	git clone https://code.google.com/p/leveldb/ || (cd leveldb; git pull);
 	cd leveldb;
 	make clean;
-	make LDFLAGS='-L../snappy-read-only/.libs/ -Bstatic -lsnappy' OPT='-fPIC -O2 -DNDEBUG -DSNAPPY -I../snappy-read-only' SNAPPY_CFLAGS=''
+	make libleveldb.a LDFLAGS='-L../snappy-read-only/.libs/ -Bstatic -lsnappy' OPT='-fPIC -O2 -DNDEBUG -DSNAPPY -I../snappy-read-only' SNAPPY_CFLAGS=''
 )
