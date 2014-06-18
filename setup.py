7d719b5af589aafec3ba49b7c5a5d712d3284b56
Mikhail Korobov <kmike84@gmail.com>
2014-06-18 23:08:06 +0600

    setup.py fixes
    * use syntax that is supported by both Python 3 and Python 2;
    * respect virtualenv in shebang;
    * remove unneeded import;
    * fix 2-space and inconsistent indentation;
    * use https URL for the repo link.

--- setup.py
+++ setup.py
@@ -1,54 +1,53 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 # Copyright (c) Arni Mar Jonsson.
-# 
+#
 # Updates to setup.py/PyPi - Russell Power (power@cs.nyu.edu)
 #
 #
 # See LICENSE for details.
 
-import glob
 import platform
 import sys
 
 from setuptools import setup, Extension
 
-system,node,release,version,machine,processor = platform.uname()
+system, node, release, version, machine, processor = platform.uname()
 common_flags = [
-      '-I./leveldb/include',
-      '-I./leveldb',
-      '-I./snappy',
-      '-I.',
-      '-fno-builtin-memcmp',
-      '-O2',
-      '-fPIC',
-      '-DNDEBUG',
-      '-DSNAPPY',
+    '-I./leveldb/include',
+    '-I./leveldb',
+    '-I./snappy',
+    '-I.',
+    '-fno-builtin-memcmp',
+    '-O2',
+    '-fPIC',
+    '-DNDEBUG',
+    '-DSNAPPY',
 ]
 
 if system == 'Darwin':
-  extra_compile_args = common_flags + [
-      '-DOS_MACOSX',
-      '-DLEVELDB_PLATFORM_POSIX',
-      '-Wno-error=unused-command-line-argument-hard-error-in-future',
-      ]
+    extra_compile_args = common_flags + [
+        '-DOS_MACOSX',
+        '-DLEVELDB_PLATFORM_POSIX',
+        '-Wno-error=unused-command-line-argument-hard-error-in-future',
+    ]
 elif system == 'Linux':
-  extra_compile_args = common_flags + [
-      '-pthread',
-      '-Wall', 
-      '-DOS_LINUX',
-      '-DLEVELDB_PLATFORM_POSIX',
-      ]
+    extra_compile_args = common_flags + [
+        '-pthread',
+        '-Wall',
+        '-DOS_LINUX',
+        '-DLEVELDB_PLATFORM_POSIX',
+    ]
 else:
-  print >>sys.stderr, "Don't know how to compile leveldb for %s!" % system
-  sys.exit(0)
+    sys.stderr.write("Don't know how to compile leveldb for %s!\n" % system)
+    sys.exit(1)
 
 setup(
 	name = 'leveldb',
 	version = '0.193',
 	maintainer = 'Arni Mar Jonsson',
 	maintainer_email = 'arnimarkj@gmail.com',
-	url = 'http://code.google.com/p/py-leveldb/',
+	url = 'https://code.google.com/p/py-leveldb/',
 
 	classifiers = [
 		'Development Status :: 4 - Beta',
@@ -75,56 +74,56 @@ setup(
 	ext_modules = [
 		Extension('leveldb',
 			sources = [
-        # snappy
-        './snappy/snappy.cc',
-        './snappy/snappy-stubs-internal.cc',
-        './snappy/snappy-sinksource.cc',
-        './snappy/snappy-c.cc',
+                # snappy
+                './snappy/snappy.cc',
+                './snappy/snappy-stubs-internal.cc',
+                './snappy/snappy-sinksource.cc',
+                './snappy/snappy-c.cc',
 
-        #leveldb
-        'leveldb/db/builder.cc', 
-        'leveldb/db/c.cc', 
-        'leveldb/db/db_impl.cc', 
-        'leveldb/db/db_iter.cc', 
-        'leveldb/db/dbformat.cc', 
-        'leveldb/db/filename.cc', 
-        'leveldb/db/log_reader.cc', 
-        'leveldb/db/log_writer.cc', 
-        'leveldb/db/memtable.cc', 
-        'leveldb/db/repair.cc', 
-        'leveldb/db/table_cache.cc', 
-        'leveldb/db/version_edit.cc', 
-        'leveldb/db/version_set.cc', 
-        'leveldb/db/write_batch.cc', 
-        'leveldb/table/block.cc', 
-        'leveldb/table/block_builder.cc', 
-        'leveldb/table/filter_block.cc', 
-        'leveldb/table/format.cc', 
-        'leveldb/table/iterator.cc', 
-        'leveldb/table/merger.cc', 
-        'leveldb/table/table.cc', 
-        'leveldb/table/table_builder.cc', 
-        'leveldb/table/two_level_iterator.cc', 
-        'leveldb/util/arena.cc', 
-        'leveldb/util/bloom.cc', 
-        'leveldb/util/cache.cc', 
-        'leveldb/util/coding.cc', 
-        'leveldb/util/comparator.cc', 
-        'leveldb/util/crc32c.cc', 
-        'leveldb/util/env.cc', 
-        'leveldb/util/env_posix.cc', 
-        'leveldb/util/filter_policy.cc', 
-        'leveldb/util/hash.cc', 
-        'leveldb/util/histogram.cc', 
-        'leveldb/util/logging.cc', 
-        'leveldb/util/options.cc', 
-        'leveldb/util/status.cc', 
-        'leveldb/port/port_posix.cc', 
+                #leveldb
+                'leveldb/db/builder.cc',
+                'leveldb/db/c.cc',
+                'leveldb/db/db_impl.cc',
+                'leveldb/db/db_iter.cc',
+                'leveldb/db/dbformat.cc',
+                'leveldb/db/filename.cc',
+                'leveldb/db/log_reader.cc',
+                'leveldb/db/log_writer.cc',
+                'leveldb/db/memtable.cc',
+                'leveldb/db/repair.cc',
+                'leveldb/db/table_cache.cc',
+                'leveldb/db/version_edit.cc',
+                'leveldb/db/version_set.cc',
+                'leveldb/db/write_batch.cc',
+                'leveldb/table/block.cc',
+                'leveldb/table/block_builder.cc',
+                'leveldb/table/filter_block.cc',
+                'leveldb/table/format.cc',
+                'leveldb/table/iterator.cc',
+                'leveldb/table/merger.cc',
+                'leveldb/table/table.cc',
+                'leveldb/table/table_builder.cc',
+                'leveldb/table/two_level_iterator.cc',
+                'leveldb/util/arena.cc',
+                'leveldb/util/bloom.cc',
+                'leveldb/util/cache.cc',
+                'leveldb/util/coding.cc',
+                'leveldb/util/comparator.cc',
+                'leveldb/util/crc32c.cc',
+                'leveldb/util/env.cc',
+                'leveldb/util/env_posix.cc',
+                'leveldb/util/filter_policy.cc',
+                'leveldb/util/hash.cc',
+                'leveldb/util/histogram.cc',
+                'leveldb/util/logging.cc',
+                'leveldb/util/options.cc',
+                'leveldb/util/status.cc',
+                'leveldb/port/port_posix.cc',
 
 				# python stuff
 				'leveldb_ext.cc',
 				'leveldb_object.cc',
-			],
+		    ],
 			libraries = ['stdc++'],
 			extra_compile_args = extra_compile_args,
 		)
